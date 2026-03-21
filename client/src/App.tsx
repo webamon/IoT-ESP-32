85f656fa060d6f2702b14dc0aeb25270f95e6145
Hamon <webamon@gmail.com>
2026-03-21 14:56:26 +0100

    rf(client): refactoring suite au changement server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,47 +1,61 @@
 import { useEffect, useState } from 'react'
-import { Legend, Line, LineChart, XAxis, YAxis } from 'recharts';
-
+import { Legend, Line, LineChart, XAxis, YAxis } from 'recharts'
+import type { SensorData } from '../../server/src/services/sensor-service'
 
 import './App.css'
 
-const WS_URI = 'ws://localhost:3000/temperature'
-
-interface Temperature {
-    value: number
-    date: EpochTimeStamp
-}
+const WS_URI = 'ws://localhost:3000/sensor-measures'
 
 function App() {
-
-  const [temperatures, setTemperatures] = useState<Temperature[]>([])
+  const [temperatures, setTemperatures] = useState<SensorData[]>([])
+  const [humidity, setHumidity] = useState<SensorData[]>([])
 
   useEffect(() => {
     const ws = new WebSocket(WS_URI)
 
     ws.addEventListener('message', (e) => {
+      const sensorDatas: Array<SensorData> = JSON.parse(e.data)
 
-      const {value, time}= JSON.parse(e.data)
+      sensorDatas.map((sensorData) => {
+        if (sensorData.metric === 'temperature') {
+          setTemperatures((prev) => [...prev, { ...sensorData }])
+        }
 
-      console.log()
-      setTemperatures(prev => [...prev, {value:value,date:time}])
+        if (sensorData.metric === 'humidity') {
+          setHumidity((prev) => [...prev, { ...sensorData }])
+        }
+      })
     })
 
     return () => ws.close()
   }, [])
 
-
-  
+  console.log(temperatures, humidity)
   return (
-  <>
-    <div>Hellow World</div>
-    {temperatures.length !==0 && <div>{temperatures[0].date}/{temperatures[0].value}</div> }
-    <LineChart style={{ width: '100%', aspectRatio: 1.618, maxWidth: 600 }} responsive data={temperatures}>
+    <>
+      <div>Hellow World</div>
+
+      <LineChart
+        style={{ width: '100%', aspectRatio: 1.618, maxWidth: 600 }}
+        responsive
+        data={temperatures}
+      >
         <Line dataKey="value" />
-        <XAxis dataKey="date" />
+        <XAxis dataKey="time" />
         <YAxis dataKey="value" />
         <Legend />
-    </LineChart>
-  </>
+      </LineChart>
+      <LineChart
+        style={{ width: '100%', aspectRatio: 1.618, maxWidth: 600 }}
+        responsive
+        data={humidity}
+      >
+        <Line dataKey="value" />
+        <XAxis dataKey="time" />
+        <YAxis dataKey="value" />
+        <Legend />
+      </LineChart>
+    </>
   )
 }
 
